c1a448840fb41dda80a408b5cf908699341b0bce
Kevin-Shih-Kai-Jen <kevin159264@gmail.com>
2026-05-05 22:01:30 +1000

    added a lot of graphs

---
+++ second-year-first-sem/data-visualization/assignment 2/codes/script.js
@@ -0,0 +1,14 @@
+const GRAPH_AMOUNT = 9
+
+for (let i = 1; i < GRAPH_AMOUNT + 1; i++){
+    const id = `#graph_${i}`
+    const data_path = `./chart_json/graph${i}.json`
+
+    document.querySelector(id).innerHTML = '';
+    
+    vegaEmbed(id, data_path)
+    .then(result => {
+        console.log("Successfully load the graph");
+    })
+    .catch(console.error);
+}
